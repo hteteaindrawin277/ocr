9f88d0f39715819cf87ecc346964390d22ce33dd
Htet Eain <hteteaindrawin7@gmail.com>
2023-05-20 08:19:36 +0630

    first

--- ocr/extract1.py
+++ ocr/extract1.py
@@ -3,6 +3,6 @@ from PIL import Image
 
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
-img=Image.open('sampletext.png')
+img=Image.open('kpay.jpg')
 text =pytesseract.image_to_string(img)
 print(text)
--- ocr/text.py
+++ ocr/text.py
@@ -2,10 +2,10 @@ import cv2
 
 import pytesseract
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
-img = cv2.imread('wave1.jpg')
+img = cv2.imread('kpay.jpg')
 img = cv2.resize(img,None,fx=0.5,fy=0.5)
 
 text=pytesseract.image_to_string(img)
 print(text)
 cv2.imshow("img",img)
-#cv2.waitkey(0)
+cv2.waitKey(0)
